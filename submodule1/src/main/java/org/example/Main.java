cdf9d9401a58d9633f0e82b9c79d204d99425189
Pavel.Krizskii <pavel.krizskii@jetbrains.com>
2025-08-05 14:31:49 +0200

    use java 8

--- submodule1/src/main/java/org/example/Main.java
+++ submodule1/src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from submodule1!");
 
-        var randomStringGenerator = RandomStringGenerator.builder().get();
+        RandomStringGenerator randomStringGenerator = RandomStringGenerator.builder().get();
         System.out.println("A random string: " + randomStringGenerator.generate(10));
     }
 }
